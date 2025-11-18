d1c5e40478daa275116d1ae5691b562c0cd8391e
llucaslopes <phyngo.lucas@hotmail.com>
2025-11-18 19:40:17 -0300

    feat: :sparkles: 2 commit

--- src/tests/Tests.spec.js
+++ src/tests/Tests.spec.js
@@ -1,50 +1,47 @@
-import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/latest/dist/bundle.js';
-import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
-import http from 'k6/http';
-import { check } from 'k6';
-import { Trend, Rate } from 'k6/metrics';
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/latest/dist/bundle.js";
+import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
+import http from "k6/http";
+import { check } from "k6";
+import { Trend, Rate } from "k6/metrics";
 
-export const getContactsDuration = new Trend('get_contacts', true);
-export const RateContentOK = new Rate('content_OK');
+export const durationGetUsers = new Trend("duration_get_users", true);
+export const successRate = new Rate("success_rate_users");
 
 export const options = {
   thresholds: {
-    http_req_failed: ['rate<0.30'],
-    get_contacts: ['p(99)<500'],
-    content_OK: ['rate>0.95']
+    http_req_failed: ["rate<0.25"],
+    duration_get_users: ["p(90)<6800"],
   },
   stages: [
-    { duration: '10s', target: 2 },
-    { duration: '10s', target: 4 },
-    { duration: '10s', target: 6 }
-  ]
+    { duration: "1m", target: 7 },
+    { duration: "1m", target: 92 },
+    { duration: "1m30s", target: 92 },
+  ],
 };
+
+const USERS_API = "https://gorest.co.in/public/v2/users";
+const STATUS_OK = 200;
+
+export default function () {
+  const requestConfig = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  const response = http.get(USERS_API, requestConfig);
+
+  durationGetUsers.add(response.timings.duration);
+  successRate.add(response.status === STATUS_OK);
+
+  check(response, {
+    "Users endpoint respondeu com 200": () => response.status === STATUS_OK,
+  });
+}
 
 export function handleSummary(data) {
   return {
-    './src/output/index.html': htmlReport(data),
-    stdout: textSummary(data, { indent: ' ', enableColors: true })
+    "report.html": htmlReport(data),
+    stdout: textSummary(data, { indent: " ", enableColors: true }),
   };
-}
-
-export default function () {
-  const baseUrl = 'https://test.k6.io/';
-
-  const params = {
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  };
-
-  const OK = 200;
-
-  const res = http.get(`${baseUrl}`, params);
-
-  getContactsDuration.add(res.timings.duration);
-
-  RateContentOK.add(res.status === OK);
-
-  check(res, {
-    'GET Contacts - Status 200': () => res.status === OK
-  });
 }
